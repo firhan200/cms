6a42e404952f24ac6f6d4211bad3ca31a422fa59
firhan200 <firhan.faisal1995@gmail.com>
2018-04-29 18:52:04 -0700

    front page landing page

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -12,19 +12,15 @@
         <div class="card-body text-white text-center py-5 px-5 my-5">
 
             <h1 class="mb-4">
-                <strong>Learn Bootstrap 4 with MDB</strong>
+                <strong>CMS</strong>
             </h1>
             <p>
-                <strong>Best & free guide of responsive web design</strong>
+                Content Management System Starter<br/>
+                Built in <strong>Laravel 5.5</strong>, <strong>Bootstrap 4.0</strong> & <strong>jQuery</strong>
             </p>
-            <p class="mb-4">
-                <strong>The most comprehensive tutorial for the Bootstrap 4. Loved by over 500 000 users. Video and written
-                    versions available. Create your own, stunning website.</strong>
-            </p>
-            <a target="_blank" href="https://mdbootstrap.com/bootstrap-tutorial/" class="btn btn-outline-white btn-lg">Start free tutorial
-                <i class="fa fa-graduation-cap ml-2"></i>
+            <a target="_blank" href="{{ url('/admin') }}" class="btn btn-outline-white btn-lg">Go To Admin
+                <i class="fa fa-link ml-2"></i>
             </a>
-
         </div>
         <!-- Content -->
     </section>
@@ -32,108 +28,83 @@
 
     <hr class="my-5">
 
-    <!--Section: Cards-->
-    <section class="text-center">
+    <!--Section: More-->
+    <section>
+    <!--First row-->
+      <div class="row features-small mt-5 wow fadeIn">
 
-        <!--Grid row-->
-        <div class="row mb-4 wow fadeIn">
-
-            <!--Grid column-->
-            <div class="col-lg-4 col-md-12 mb-4">
-
-                <!--Card-->
-                <div class="card">
-
-                    <!--Card image-->
-                    <div class="view overlay">
-                        <div class="embed-responsive embed-responsive-16by9 rounded-top">
-                            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/cXTThxoywNQ" allowfullscreen></iframe>
-                        </div>
-                    </div>
-
-                    <!--Card content-->
-                    <div class="card-body">
-                        <!--Title-->
-                        <h4 class="card-title">MDB Quick Start</h4>
-                        <!--Text-->
-                        <p class="card-text">Get started with MDBootstrap, the world's most popular Material Design framework for building
-                            responsive, mobile-first sites.</p>
-                        <p class="card-text">
-                            <strong>5 minutes, a few clicks and... done. You will be surprised at how easy it is.</strong>
-                        </p>
-                    </div>
-
-                </div>
-                <!--/.Card-->
-
+        <!--Grid column-->
+        <div class="col-xl-3 col-lg-6">
+          <!--Grid row-->
+          <div class="row">
+            <div class="col-2">
+              <i class="fa fa-cog fa-2x mb-1 indigo-text" aria-hidden="true"></i>
             </div>
-            <!--Grid column-->
-
-            <!--Grid column-->
-            <div class="col-lg-4 col-md-6 mb-4">
-
-                <!--Card-->
-                <div class="card">
-
-                    <!--Card image-->
-                    <div class="view overlay">
-                        <img src="https://mdbootstrap.com/wp-content/uploads/2017/11/brandflow-tutorial-fb.jpg" class="card-img-top" alt="">
-                        <a href="https://mdbootstrap.com/automated-app-start/" target="_blank">
-                            <div class="mask rgba-white-slight"></div>
-                        </a>
-                    </div>
-
-                    <!--Card content-->
-                    <div class="card-body">
-                        <!--Title-->
-                        <h4 class="card-title">Bootstrap Automation</h4>
-                        <!--Text-->
-                        <p class="card-text">Learn how to create a smart website which learns your user and reacts properly to his behavior.</p>
-                        <a href="https://mdbootstrap.com/automated-app-start/" target="_blank" class="btn btn-primary btn-md">Start tutorial
-                            <i class="fa fa-play ml-2"></i>
-                        </a>
-                    </div>
-
-                </div>
-                <!--/.Card-->
-
+            <div class="col-10 mb-2 pl-3">
+              <h5 class="feature-title font-bold mb-1">Easy CRUD</h5>
+              <p class="grey-text mt-2">simple create, read, update & delete table object.
+              </p>
             </div>
-            <!--Grid column-->
-
-            <!--Grid column-->
-            <div class="col-lg-4 col-md-6 mb-4">
-
-                <!--Card-->
-                <div class="card">
-
-                    <!--Card image-->
-                    <div class="view overlay">
-                        <img src="https://mdbootstrap.com/wp-content/uploads/2018/01/push-fb.jpg" class="card-img-top" alt="">
-                        <a href="https://mdbootstrap.com/web-push-start/" target="_blank">
-                            <div class="mask rgba-white-slight"></div>
-                        </a>
-                    </div>
-
-                    <!--Card content-->
-                    <div class="card-body">
-                        <!--Title-->
-                        <h4 class="card-title">Push notifications</h4>
-                        <!--Text-->
-                        <p class="card-text">Push messaging provides a simple and effective way to re-engage with your users and in this
-                            tutorial you'll learn how to add push notifications to your web app</p>
-                        <a href="https://mdbootstrap.com/web-push-start/" target="_blank" class="btn btn-primary btn-md">Start tutorial
-                            <i class="fa fa-play ml-2"></i>
-                        </a>
-                    </div>
-
-                </div>
-                <!--/.Card-->
-
-            </div>
-            <!--Grid column-->
-
+          </div>
+          <!--/Grid row-->
         </div>
-        <!--Grid row-->
+        <!--/Grid column-->
+
+        <!--Grid column-->
+        <div class="col-xl-3 col-lg-6">
+          <!--Grid row-->
+          <div class="row">
+            <div class="col-2">
+              <i class="fa fa-area-chart fa-2x mb-1 indigo-text" aria-hidden="true"></i>
+            </div>
+            <div class="col-10 mb-2">
+              <h5 class="feature-title font-bold mb-1">Elegant Dashboard</h5>
+              <p class="grey-text mt-2">simple & elegant dashboard
+              </p>
+            </div>
+          </div>
+          <!--/Grid row-->
+        </div>
+        <!--/Grid column-->
+
+        <!--Grid column-->
+        <div class="col-xl-3 col-lg-6">
+          <!--Grid row-->
+          <div class="row">
+            <div class="col-2">
+              <i class="fa fa-github fa-2x mb-1 indigo-text" aria-hidden="true"></i>
+            </div>
+            <div class="col-10 mb-2">
+              <h5 class="feature-title font-bold mb-1">Open Source</h5>
+              <p class="grey-text mt-2">available on github
+              </p>
+            </div>
+          </div>
+          <!--/Grid row-->
+        </div>
+        <!--/Grid column-->
+
+        <!--Grid column-->
+        <div class="col-xl-3 col-lg-6">
+          <!--Grid row-->
+          <div class="row">
+            <div class="col-2">
+              <i class="fa fa-code fa-2x mb-1 indigo-text" aria-hidden="true"></i>
+            </div>
+            <div class="col-10 mb-2">
+              <h5 class="feature-title font-bold mb-1">jQuery 3.x</h5>
+              <p class="grey-text mt-2">using mainstream loveable javascript library
+              </p>
+            </div>
+          </div>
+          <!--/Grid row-->
+        </div>
+        <!--/Grid column-->
+
+      </div>
+      <!--/First row-->
+
     </section>
+    <!--Section: More-->
 </div>
 @endsection
